29b4a90bc431cffc5bffc029bb2d62822845dadc
Ishwar Chaudhary <ishwarura@gmail.com>
2025-03-16 23:18:29 +0545

    URL Updates

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getAuthHeaders } from "./authService";
 
-const API_URL = "http://localhost:5500/api/admin/orders";
+const API_URL = "https://epharmacy-api.onrender.com/api/admin/orders";
 
 export const getOrders = async () => {
     const response = await axios.get(API_URL, getAuthHeaders());
